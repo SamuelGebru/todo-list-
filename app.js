8547d40b50d7aca70d97a655f07363aa3f6fab83
Samuel Gebru <samueltgebru@gmail.com>
2022-07-28 11:12:52 +0100

    created first  logic inside the ejs

---
+++ app.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
+
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res) {
+
+  res.sendFile(__dirname + '/index.html') // respond from our server to the client
+
+  var today = new Date();
+  var currentDay = today.getDay();
+  var day = "";
+
+  switch (currentDay) {
+    case 0:
+      day = "Sunday";
+      break;
+    case 1:
+      day = "Monday";
+      break;
+    case 2:
+      day = "Tuesday";
+      break;
+    case 3:
+      day = "Wednesday";
+      break;
+    case 4:
+      day = "Thursday";
+      break;
+    case 5:
+      day = "Friday";
+      break;
+    case 6:
+      day = "Saturday";
+      break;
+    default: console.log("Error: current day is equal to: " + currentDay);
+ }
+  res.render("list", {
+    kindOfDay: day
+  });
+});
+
+app.post('/', function(req, res) {
+
+});
+
+app.listen(3000, function() {
+  console.log("Listening on port 3000");
+})
