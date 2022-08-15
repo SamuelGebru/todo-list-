7404cbd7796ce5c07d5b0e7bd0a9b6f723cf2c45
Samuel Gebru <samueltgebru@gmail.com>
2022-08-15 01:03:30 +0100

    added a module called date

--- app.js
+++ app.js
@@ -1,44 +1,51 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const date = require(__dirname + "/date.js");
 
 const app = express();
 app.use(bodyParser.urlencoded({
   extended: true
 }));
 
-app.use(express.static('public'));
+app.use(express.static('public')); // specify the location of the public folder
 
 let items = ["Buy food", "Cook food", "Eat food"];
+let workItems = [];
 
 app.set('view engine', 'ejs');
 
 app.get('/', function(req, res) {
 
-  res.sendFile(__dirname + '/index.html') // respond from our server to the client
-
-  let today = new Date();
-
-  let options = {
-    weekday: "long",
-    day: "numeric",
-    month: "long"
-  };
-
-  let day = today.toLocaleDateString("en-US", options);
+  let day = date();
 
   res.render("list", {
-    kindOfDay: day, newListItems: items
+    listTitle: day, newListItems: items
   });
 });
 
 app.post('/', function(req, res) {
-  console.log(req.body.toDoList);
+  console.log(req.body);
   let item = req.body.toDoList;
 
-  items.push(item)
-
-  res.redirect("/")
+  if (req.body.list === "Work") {
+    workItems.push(item)
+    res.redirect("/work")
+  } else {
+    items.push(item)
+    res.redirect("/")
+  }
 });
+
+app.get('/work', function(req, res) {
+
+  res.render("list", {
+    listTitle: "Work List", newListItems: workItems});
+});
+
+app.get("/about", function(req,res) {
+  res.render("about");
+})
+
 
 app.listen(3000, function() {
   console.log("Listening on port 3000");
